c18f673857b9ccd31db03dc73904313d25ab8414
SergeyNevinskiy <s.nevinskiy1987@ukr.net>
2022-07-03 22:01:52 +0300

    Update

--- scipt/script.js
+++ scipt/script.js
@@ -3,9 +3,26 @@
 const num1 = +prompt('Enter number');
 const num2 = +prompt('Enter number');
 
-const add = num1 + num2;
-const sub = num1 - num2;
-const mult = num1 * num2;
-const div = num1 / num2;
+function sum() {
+   const result = num1 + num2;
+   console.log(`${num1} + ${num2} = ${result}`)
+}
+sum()
 
-console.log(add, sub, mult, div)
+function sub() {
+   const result = num1 - num2;
+   console.log(`${num1} - ${num2} = ${result}`)
+}
+sub()
+
+function div() {
+   const result = num1 / num2;
+   console.log(`${num1} / ${num2} = ${result}`)
+}
+div()
+
+function mult() {
+   const result = num1 * num2;
+   console.log(`${num1} * ${num2} = ${result}`)
+}
+mult()
