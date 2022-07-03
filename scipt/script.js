e34d154a8e2a1ccde49fa1b4d825dbd066232392
SergeyNevinskiy <s.nevinskiy1987@ukr.net>
2022-07-03 11:48:42 +0300

    Ok

---
+++ scipt/script.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const num1 = +prompt('Enter number');
+const num2 = +prompt('Enter number');
+
+const add = num1 + num2;
+const sub = num1 - num2;
+const mult = num1 * num2;
+const div = num1 / num2;
+
+console.log(add, sub, mult, div)
